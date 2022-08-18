1c526ef48883a0a26e24462c40bee5251c1d4871
Adamor Henner <adacandido9@gmail.com>
2022-08-18 19:36:44 -0300

    first commit

---
+++ src/main/java/br/com/ijuda/api/IjudaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ijuda.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjudaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjudaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ijuda/api/IjudaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ijuda.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjudaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
