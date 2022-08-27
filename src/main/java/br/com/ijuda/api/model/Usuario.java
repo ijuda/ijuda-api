2ffa2341e01cc0ca9307dbe93c402b9768482126
Adamor Henner <adacandido9@gmail.com>
2022-08-27 19:39:38 -0300

    Inserindo configuracoes de autenticacao

---
+++ src/main/java/br/com/ijuda/api/model/Usuario.java
@@ -0,0 +1,55 @@
+package br.com.ijuda.api.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "usuario")
+@SequenceGenerator(name = "SEQUENCE", sequenceName = "usuario_id_seq", allocationSize = 1)
+public class Usuario {
+
+	@Id
+	private Long codigo;
+
+	private String nome;
+	private String email;
+	private String senha;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "usuario_permissao", joinColumns = @JoinColumn(name = "codigo_usuario")
+		, inverseJoinColumns = @JoinColumn(name = "codigo_permissao"))
+	private List<Permissao> permissoes;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
